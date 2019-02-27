1456943e1ceff3e52b3781f3fb49616a0f874582
epitek <arkadiusz@bubak.pl>
2019-02-27 11:37:47 +0100

    Changes in LStaticFunctionMember.cc

--- src/LStaticFunctionMember.cc
+++ src/LStaticFunctionMember.cc
@@ -1,10 +1,12 @@
-/*By declaring a function member as static, you make it independent of any particular object of the class.
-A static member function can be called even if no objects of the class exist and the static functions are
-accessed using only the class name and the scope resolution operator ::
-A static member function can only access static data member, other static member functions and any other
-functions from outside the class. Static member functions have a class scope and they do not have access
-to the this pointer of the class. You could use a static member function to determine whether some objects
-of the class have been created or not.*/
+/*By declaring a function member as static, you make it independent of any
+particular object of the class. A static member function can be called even
+if no objects of the class exist and the static functions are accessed using
+only the class name and the scope resolution operator ::. A static member
+function can only access static data member, other static member functions
+and any other functions from outside the class. Static member functions have
+a class scope and they do not have access to the this pointer of the class.
+You could use a static member function to determine whether some objects of
+the class have been created or not.*/
 
 #include <iostream>
  
