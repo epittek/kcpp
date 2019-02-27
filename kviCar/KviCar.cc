be7803008518301bdf310c29d2bdf9de9a82f5fc
epitek <arkadiusz@bubak.pl>
2019-02-27 13:42:44 +0100

    Added class example KviCar

---
+++ kviCar/KviCar.cc
@@ -0,0 +1,9 @@
+/* Author: Arkadiusz Bubak
+   mail: arkadiusz@buabk.pl
+   date: 2008.02.22
+ */
+ 
+#include "KviCar.h"
+
+KviCar::KviCar()  {};
+KviCar::~KviCar() {};
---
+++ kviCar/KviVan.cxx
@@ -0,0 +1,22 @@
+/* Author: Arkadiusz Bubak
+   mail: arkadiusz@buabk.pl
+   date: 2008.02.22
+ */
+ 
+#include "KviVan.h"
+#include <iostream>
+using namespace std;
+
+
+KviVan::KviVan() {
+  //cout <<"The KviVan Object has been created" << endl;
+};
+KviVan::~KviVan() {
+  //cout <<"The KviVan Object has been destroyed" << endl;
+};
+
+void KviVan::StolePlate() {
+
+  cout <<"I am bad descendant. I use this function to stole the plate"<< endl;
+
+}
