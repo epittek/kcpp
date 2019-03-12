7d2b6db086a577faa6469a9097fdf2d6f253de3f
epitek <arkadiusz@bubak.pl>
2019-03-12 15:24:20 +0100

    New file

---
+++ kcppBasic/src/LArytmetykaWskaznikowTablice2.cc
@@ -0,0 +1,46 @@
+//#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+#define ROZMIAR 10
+
+//--- dostep za pomoca indeksu --------------------------------------
+void funA(){
+
+   int tab[ROZMIAR];
+   int i; 
+   
+   for(i = 0; i < ROZMIAR; i++) {
+	   cout <<"Wpisz wartosc: ";
+       cin >> tab[i];
+       //scanf("%d", &tab[i]);
+       tab[i] = 2 * tab[i];  // tab[i] *= 2;
+       cout <<"Tab["<<i<<"] = "<< tab[i] << endl;
+       //printf("Tab[%d] = %d \n", i+1 , tab[i]);
+   }
+}
+
+
+//--- dostep za pomoca adresu i indeksu ----------------------------
+void funB(){
+
+   int tab[ROZMIAR];
+   int i; 
+   int k;
+   for(i = 0; i < ROZMIAR; i++) {
+
+       //scanf("%d", tab+i);      //&*(tab+i) == tab+i
+	   cout <<"Wpisz wartosc: ";
+       cin >> *(tab+i);
+       *(tab+i) = 2 * *(tab+i);  // tab[i] *= 2;
+       cout <<"Tab["<<i<<"] = "<< *(tab+i) << endl;
+       //printf("Tab[%d] = %d \n", i+1 , *(tab+i));
+   }
+}
+
+
+
+int main()
+{
+  funA();
+}
