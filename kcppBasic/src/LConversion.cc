0c35041ce560c1d0acd540ea0592413068120d36
Arkadiusz Bubak <abubak@155.158.122.205>
2019-03-20 09:31:58 +0100

    Added hextodec function

--- kcppBasic/src/LConversion.cc
+++ kcppBasic/src/LConversion.cc
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <stdint.h>
+#include <math.h>
 
 using namespace std;
 
@@ -40,11 +41,34 @@ string bintohex(const string &s){
 }
 
 
+unsigned long hextodec(string hex)
+{
+    unsigned long result = 0;
+    for (unsigned int i=0; i<hex.length(); i++) {
+        if (hex[i]>=48 && hex[i]<=57)
+        {
+            result += (hex[i]-48)*pow(16,hex.length()-i-1);
+        } else if (hex[i]>=65 && hex[i]<=70) {
+            result += (hex[i]-55)*pow(16,hex.length( )-i-1);
+        } else if (hex[i]>=97 && hex[i]<=102) {
+            result += (hex[i]-87)*pow(16,hex.length()-i-1);
+        }
+    }
+    return result;
+}
+
+
 int main(){
 
+    string s[] = {"0x7ffeca8c8c58", "0x7ffc5ffa893c", "0x7ffc5ffa8940"};
+    cout << hextobin(s[0]) << endl;
+    cout << hextobin(s[1]) << endl;
+    cout << hextobin(s[2]) << endl;
 
-    string s1 = "0x7ffeca8c8c58";
-    cout << hextobin(s1) << endl;
+    cout << hextodec(s[0]) << endl;
+    cout << hextodec(s[1]) << endl;
+    cout << hextodec(s[2]) << endl;
+
     return 0;
 }
 
