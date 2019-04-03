3cd5c453b4901d985852cfe01915951eefe36354
epitek <arkadiusz@bubak.pl>
2019-04-03 10:30:55 +0200

    Przyklad strumieni (uzupelnienie)

--- kcppBasic/src/LStrumienie.cc
+++ kcppBasic/src/LStrumienie.cc
@@ -4,18 +4,58 @@
  */
 
 #include <iostream>
+#include <string>
 using namespace std;
+
+
+void InputZeSpacjami(){
+
+    cout << "Podaj tekst: ";
+    string tekst;
+    getline(cin, tekst);
+    cout << tekst << endl;
+
+}
+
+
+void InputZeSpacjamiPoprawny(){
+
+    //Jeśli przed użyciem funkcji "getline" użyjemy obiektu "cin", ten ostatni pozostawia zwykle znak końca wiersza '\n' w buforze klawiatury. Funkcja "getline" napotykając ten znak natychmiast kończy działanie, więc żeby uniknąć błędnego działania programu, należy wywołać funkcję cin.ignore().
+
+    //cout << "Podaj liczbę: ";
+    //int liczba;
+    //cin >> liczba;
+
+    cout << "Podaj tekst: ";
+    string tekst;
+    cin.ignore(); // to wywołanie usunie z bufora znak '\n' pozostawiony przez obiekt "cin"
+    getline(cin, tekst);
+
+    cout  << tekst << endl;
+    //cout << liczba << ' ' << tekst << endl;
+
+}
+
 
 int main(){
 
     cout <<"Test standardowego strumienia"<< endl;
     cerr <<"Test strumienia bledow"<< endl;
 
-    int wejscie;
+    int A, B, C;
 
     cout <<"Podaj wartosc: ";
-    cin >> wejscie;
-    cout <<"Wpisales: "<< wejscie << endl;
+    cin >> A;
+    cout <<"Wpisales: "<< A << endl;
+
+
+    cout <<"Podaj 2 wartosci: ";
+    cin >> B >> C;
+    cout <<"Wpisales: "<< A <<"\t"<< B << endl;
+
+
+    InputZeSpacjami();
+    //InputZeSpacjamiPoprawny();
 
     return 0;
 }
