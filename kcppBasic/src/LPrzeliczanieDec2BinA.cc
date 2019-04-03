87fe95170b474107e246a983ff4fcda6e0ebf9e4
epitek <arkadiusz@bubak.pl>
2019-04-03 09:30:11 +0200

    Binary to decimal

---
+++ kcppBasic/src/LPrzeliczanieDec2BinA.cc
@@ -0,0 +1,33 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+void dec_to_bin(int liczba)
+{
+    int i=31;
+    bool ok=false;
+    while(i--)
+    {
+        //warunek, który pozwoli ominąć początkowe zera
+        if(liczba>>i&1&!ok) 
+            ok=true;
+
+        if(ok)
+            cout<<((liczba>>i)&1);
+
+    }
+}
+
+int main()
+{
+    int liczba;
+    cout<<"Podaj liczbę: ";
+    cin>>liczba;
+
+    cout<<liczba<<" po zamianie na postać binarną: ";
+    dec_to_bin(liczba);
+    cout<<endl;
+
+    system("pause");
+    return 0;
+} 
---
+++ kcppBasic/src/LPrzeliczanieDec2BinB.cc
@@ -0,0 +1,32 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+void dec_to_bin(int liczba)
+{
+    int i=0,tab[31];
+
+    while(liczba) //dopóki liczba będzie różna od zera
+    {
+        tab[i++]=liczba%2;
+        liczba/=2;
+    }
+
+    for(int j=i-1;j>=0;j--)
+        cout<<tab[j];
+}
+
+int main()
+{
+    int liczba;
+
+    cout<<"Podaj liczbę: ";
+    cin>>liczba;
+
+    cout<<liczba<<" po zamianie na postać binarną: ";
+    dec_to_bin(liczba);
+    cout<<endl;
+
+    system("pause");
+    return 0;
+}
