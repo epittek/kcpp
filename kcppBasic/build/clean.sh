e0e898028635d08232c194d3c966c6e003fc0e83
epitek <arkadiusz@bubak.pl>
2019-04-03 11:19:17 +0200

    Cosmetic corrections

--- kcppBasic/build/clean.sh
+++ kcppBasic/build/clean.sh
@@ -21,7 +21,7 @@ make clean
 #rm -rf Makefile {CM,cm}ake*
 
 printf "%s\n" "rm -rf EVERYTHING!......."
-rm -rf Makefile [Cc][Mm]ake*
+rm -rf Makefile [Cc][Mm]ake* log.txt xor.ppm
 
-progress-bar 10 # ;)
+#progress-bar 10 # ;)
 
