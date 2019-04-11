ee5b2c0f517ea38476eb6d592b514fc91c8ed68b
epitek <arkadiusz@bubak.pl>
2019-04-11 09:47:36 +0200

    ...

--- kcppBasic/src/LInstrukcjaWhile.cc
+++ kcppBasic/src/LInstrukcjaWhile.cc
@@ -8,24 +8,25 @@ using namespace std;
 
 int main()
 {   
-   int i = 5;
-   cout << i << endl;
-   
-   while(i < 10) i++; //po wykonaniu petli i = 10
-   cout << i << endl;
-   
-   while(i++) if(i==15) break; //jeślisi wynosi 15 to przerywamy wykonywanie petli
+  int i = 5;
+  cout << i << endl;
 
-   while(true) goto etykieta1; //skok do etykiety
-   cout <<"Czy kiedykolwiek sie pojawie?"<< endl;
-   
-   etykieta1:
+  while(i < 10) i++; //po wykonaniu petli i = 10
+  cout << i << endl;
 
-   while(i <= 20) { //wyswietla liczby 15, 16, 17, 19 i 20
-     if(i==18) {
-	i++; continue; 
-     }
-     cout << i++ <<" ";
-   }
-   cout << endl;
+  while(i++) if(i==15) break; //jesli wynosi 15 to przerywamy wykonywanie petli
+
+  while(true) goto etykieta1; //skok do etykiety
+  cout <<"Czy kiedykolwiek sie pojawie?"<< endl;
+
+etykieta1:
+
+  while(i <= 20) { //wyswietla liczby 15, 16, 17, 19 i 20
+    if(i==18) {
+      i++;
+      continue; 
+    }
+    cout << i++ <<" ";
+  }
+  cout << endl;
 }
