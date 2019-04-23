f0b2525a848a5b61ecbc91d11e4b6911a8289d4c
epitek <arkadiusz@bubak.pl>
2019-04-23 16:23:03 +0200

    sc

--- kcppBasic/src/LClassZagniezdzanie.cc
+++ kcppBasic/src/LClassZagniezdzanie.cc
@@ -3,28 +3,28 @@ using namespace std;
 
 class Samochod
 {
- public:
-   int fZmienna;
-   
-   class Akcje
-     {
+  public:
+    int fZmienna;
+
+    class Akcje
+    {
       public:
-	int fZmienna;
-     };
+        int fZmienna;
+    };
 };
 
 
 int main()
 {
-   Samochod Volvo;
-   Volvo.fZmienna = 7;
-  
-   //Akcje Trab;                 //Error
-   //Trab.fZmienna = 5;         //Error
-   
-   Samochod::Akcje Trab;
-   Trab.fZmienna = 5;
-   
-   cout << Volvo.fZmienna << endl;
-   cout << Trab.fZmienna  << endl;
+  Samochod Volvo;
+  Volvo.fZmienna = 7;
+
+  //Akcje Trab;                 //Error
+  //Trab.fZmienna = 5;         //Error
+
+  Samochod::Akcje Trab;
+  Trab.fZmienna = 5;
+
+  cout << Volvo.fZmienna << endl;
+  cout << Trab.fZmienna  << endl;
 }
