1b6287ffe0ae9a106eca10476b3c1e57e415f64a
epitek <arkadiusz@bubak.pl>
2019-04-24 09:32:46 +0200

    Cleaning

--- kcppBasic/src/LNawiasy.cc
+++ kcppBasic/src/LNawiasy.cc
@@ -16,15 +16,15 @@ int main() {
   int var4, var5, var6;
 
   {
-    int var1 = 7;              ///< Przesloniecie poprzedniej definicji var1
-    cout << var1 << endl;      ///< Na ekranie: 7
+    int var1 = 7;              //!< Przesloniecie poprzedniej definicji var1
+    cout << var1 << endl;      //!< Na ekranie: 7
   }
 
-  cout << var1 << endl;         ///< Na ekranie: 1
+  cout << var1 << endl;         //!< Na ekranie: 1
 
   //----------------------------------------------------   
 
-  if(var1 > 0) { ///<Instrukcja: Sprawdzanie czy var1 > 0
+  if(var1 > 0) { //!< Instrukcja: Sprawdzanie czy var1 > 0
     cout <<"Podaj nowa wartosc: ";
     cin >> var1;
   }   
