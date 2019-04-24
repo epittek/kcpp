8dfb743eb3191fc3e4eb19b0fd79a008f800361b
epitek <arkadiusz@bubak.pl>
2019-04-24 09:26:11 +0200

    added doxygen desctiption

--- kcppBasic/src/LNawiasy.cc
+++ kcppBasic/src/LNawiasy.cc
@@ -16,11 +16,11 @@ int main() {
   int var4, var5, var6;
 
   {
-    int var1 = 7;              ///<Przesloniecie poprzedniej definicji var1
-    cout << var1 << endl;      ///<Na ekranie: 7
+    int var1 = 7;              ///< Przesloniecie poprzedniej definicji var1
+    cout << var1 << endl;      ///< Na ekranie: 7
   }
 
-  cout << var1 << endl;         ///<Na ekranie: 1
+  cout << var1 << endl;         ///< Na ekranie: 1
 
   //----------------------------------------------------   
 
