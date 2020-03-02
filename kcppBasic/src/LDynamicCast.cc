9e32fd2cccbafaf651a9ec8ecec0ba84840f96c5
Arkadiusz Bubak <arkadiusz@bubak.pl>
2020-03-02 08:27:37 +0100

    new file with dynamic_cast example

---
+++ kcppBasic/src/LDynamicCast.cc
@@ -0,0 +1,35 @@
+/**
+ * \author: Arkadiusz Bubak arkadiusz@bubak.pl
+ * \date 2020.03.02
+ * \brief Konwersja: dynamic_cast w C++
+ * v0.01
+ */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//-----------------------------
+class Car {};
+class Ford : public Car {};
+
+//-----------------------------
+//
+//-----------------------------
+struct Base { };
+struct Derived : Base { };
+//-----------------------------
+
+int main(){
+  Ford *fordTaurus = new Ford();
+  Car *car = dynamic_cast<Car*>(fordTaurus);
+     // . . .
+  delete car;
+
+  Derived d; Base *b = &d;
+  //dynamic_cast<Derived*>(b); // Invalid
+
+  return 0;
+}
+
+
