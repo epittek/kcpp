af2d61fb6058f7c8ac63076f0462fd0977191b65
Arkadiusz Bubak <arkadiusz@bubak.pl>
2020-03-05 15:27:23 +0100

    Extern C in C++, mangle, unmangle example

---
+++ kcppBasic/src/LExtern.cc
@@ -0,0 +1,41 @@
+/**
+ * \author: Arkadiusz Bubak arkadiusz@bubak.pl
+ * \date 2020.03.05
+ * \brief Extern "C" C++
+ * v0.01
+ */
+
+#include<iostream>
+using namespace std;
+
+
+void a() {}
+void b() {}
+
+extern "C" {
+  void ca() {}
+  void cb() {}
+}
+
+/* Zapobiegnij optymalizacji funkcji g i eg */
+void h()
+{
+  b(); cb();
+}
+
+
+int main()
+{
+  cout <<"Samodzielna kompilacja:"<< endl;
+  cout <<"  g++ -c -std=c++11 -Wall -Wextra -pedantic -o LExtern.o ../src/LExtern.c"<< endl; 
+  cout <<"Rozmontowanie/odwiklanie (diassemble):"<< endl;
+  cout <<"  readelf -s LExtern.o\n" << endl;
+
+  cout <<"Lub jesli z uzyciem cmake:"<< endl;
+  cout <<"  readelf -s CMakeFiles/LExtern.dir/src/LExtern.cc\n"<< endl;
+
+  cout <<"'Unmangle' nazw funkcji:"<< endl;
+  cout <<"  c++filt _Z1fv"<< endl;
+
+  return 0;                              
+}
