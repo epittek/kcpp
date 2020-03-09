744da99bde874f29922f47d4ee9b425c164a7685
Arkadiusz Bubak <arkadiusz@bubak.pl>
2020-03-09 14:09:03 +0100

    Added second method: how to get size of array without sizeof

--- kcppBasic/src/LCoutArray.cc
+++ kcppBasic/src/LCoutArray.cc
@@ -12,10 +12,17 @@
  
 int main()
 {
-   int  array[] = {1,2,3,4,5,6,7};
+   int  iArray[] = {1,2,3,4,5,6,7};
 
-   /* int size = sizeof(array)/sizeof(array[0]);   
-    * std::copy(array, array+size, std::ostream_iterator<int>(std::cout," ")); */
-   std::copy(std::begin(array), std::end(array), std::ostream_iterator<int>(std::cout," "));
+   //int iSize = sizeof(array)/sizeof(array[0]);
+   int iSize = *(&iArray + 1) - iArray;
+   std::copy(iArray, iArray+iSize, std::ostream_iterator<int>(std::cout," "));
+   //std::copy(std::begin(iArray), std::end(iArray), std::ostream_iterator<int>(std::cout," "));
    std::cout << std::endl;
+
+   /* Wyjasnienie drugiego sposoby dostanie wielkosci tablicy bez uzycia sizeof
+    * &arr              ==> Pointer to an array of 6 elements.
+    * (&arr + 1)        ==> Address of 6 integers ahead as pointer type is pointer to array of X integers.
+    * *(&arr + 1)       ==> Same address as (&arr + 1), but type of pointer is "int *"
+    * *(&arr + 1) - arr ==> Since *(&arr + 1) points to the address X integers ahead of arr, the difference  between two is X      */
 }
