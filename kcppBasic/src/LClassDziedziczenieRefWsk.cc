7425759a0c14e8f3bdd607bfa9d4f1478901b099
epitek <arkadiusz@bubak.pl>
2020-05-04 10:19:05 +0200

    Back to initial state

--- kcppBasic/src/LClassDziedziczenieRefWsk.cc
+++ kcppBasic/src/LClassDziedziczenieRefWsk.cc
@@ -9,41 +9,41 @@
 using namespace std;
 
 class A {
-public:
-	void Funkcja() { cout <<"Metoda z klasy A"<< endl; }
+  public:
+    void Funkcja() { cout <<"Metoda z klasy A"<< endl; }
 };
 
 class B : public A {
-public:
-  void Funkcja() { cout <<"Metoda z klasy B"<< endl; } // metoda zredefiniowana
+  public:
+    void Funkcja() { cout <<"Metoda z klasy B"<< endl; } // metoda zredefiniowana
 };
 
 int main() {
 
-   A a;
-   B b;
-   
-   
+  A a;
+  B b;
+
+
 #define ZMIANA
 
 #ifdef ZMIANA
-   A *p1 = &a;
-   B *p2 = &b;
-   A &r1 = a;
-   B &r2 = b;
+  A *p1 = &a;
+  B *p2 = &b;
+  A &r1 = a;
+  B &r2 = b;
 #else  
-   A *p1 = &a;
-   A *p2 = &b;
-   A &r1 = a;
-   A &r2 = b;
+  A *p1 = &a;
+  A *p2 = &b;
+  A &r1 = a;
+  A &r2 = b;
 #endif
-   
-   a.Funkcja();
-   b.Funkcja();
-   p1->Funkcja();
-   p2->Funkcja();
-   r1.Funkcja();
-   r2.Funkcja();
+
+  a.Funkcja();
+  b.Funkcja();
+  p1->Funkcja();
+  p2->Funkcja();
+  r1.Funkcja();
+  r2.Funkcja();
 
 
 };
