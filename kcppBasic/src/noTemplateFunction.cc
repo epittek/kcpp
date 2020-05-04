6b07fa520410402e530ace24a8b4014bc1b1d430
epitek <arkadiusz@bubak.pl>
2020-05-04 10:15:37 +0200

    Back to initial state

--- kcppBasic/src/noTemplateFunction.cc
+++ kcppBasic/src/noTemplateFunction.cc
@@ -9,21 +9,21 @@
 using namespace std;
 
 
-/* int AddInt(int t1, int t2) {
- *    return t1 +t2;
- * }
+/*   int AddInt(int t1, int t2) {
+ *      return t1 +t2;
+ *   }
  *
- * float AddFloat(float t1, float t2) {
+ *   float AddFloat(float t1, float t2) {
  *    return t1 +t2;
- * } */
+ *   } */
 
-int Add(int t1, int t2) {
-   return t1 +t2;
-}
+  int Add(int t1, int t2) {
+     return t1 +t2;
+  }
 
-float Add(float t1, float t2) {
-   return t1 +t2;
-}
+  float Add(float t1, float t2) {
+     return t1 +t2;
+  }
 
 
 int main() {
@@ -33,9 +33,9 @@ int main() {
     * cout << AddInt(2,13)    << endl;  // wywolanie szablonu max<int> (dedukcja typu argumentów)
     * cout << AddFloat(2,14)    << endl;  // wywolanie szablonu max<double> (bez dedukcji typu) */
 
-   /* cout << Add(2,3)     << endl;
-    * cout << Add(2.6,3.2) << endl;  // wywolanie szablonu max<double> (dedukcja typu argumentów)
-    * cout << Add(2,13)    << endl;  // wywolanie szablonu max<int> (dedukcja typu argumentów)
-    * cout << Add(2,14)    << endl;  // wywolanie szablonu max<double> (bez dedukcji typu) */
+   cout << Add(2,3)     << endl;
+   cout << Add(2.6,3.2) << endl;  // wywolanie szablonu max<double> (dedukcja typu argumentów)
+   cout << Add(2,13)    << endl;  // wywolanie szablonu max<int> (dedukcja typu argumentów)
+   cout << Add(2,14)    << endl;  // wywolanie szablonu max<double> (bez dedukcji typu)
     
 }
